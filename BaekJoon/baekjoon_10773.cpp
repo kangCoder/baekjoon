954f948d3f151f50455189d08c3d53404a78bf6b
coderkang <khsofficial1213@gmail.com>
2021-08-18 15:50:22 +0900

    10773 스택기본응용

---
+++ BaekJoon/baekjoon_10773.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	int K;
+	cin >> K;
+	stack<int> s;
+
+	while (K--) {
+		int n;
+		cin >> n;
+		if (n == 0)
+			s.pop();
+		else
+			s.push(n);
+	}
+
+	int sum = 0;
+	while (!s.empty()) {
+		sum += s.top();
+		s.pop();
+	}
+
+	cout << sum << endl;
+	return 0;
+}
